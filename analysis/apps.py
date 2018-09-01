51dc593ba9cb41e462642b7aeb15b97256c6bf14
rahulkumaran <rahuldravid313@gmail.com>
2018-09-01 17:43:05 +0530

    reset fork to 1ae49d8e5d71a3b9ff135b2d437d15e656c052c8 commit

--- analysis/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnalysisConfig(AppConfig):
-    name = 'analysis'
--- analysis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name="index"),
-    # url(r'^api/(?P<pk1>\d+)/(?P<state>.+?)/$', views.get_resp, name="get_response"),
-    url(r'^temp/$',views.formV,name="form")
-]
